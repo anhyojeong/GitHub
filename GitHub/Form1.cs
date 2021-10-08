37b00926dd0ec9efca7e7f7cf6a5849ecdd88b66
효정 안 <zzoo30@naver.com>
2021-10-08 14:47:45 +0900

    button1 fuction(Click) added

--- GitHub/Form1.cs
+++ GitHub/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼을 눌렀습니다.");      
+        }
     }
 }
