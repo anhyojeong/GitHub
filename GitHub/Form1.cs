103ea4efb4b57e27d95d1423c165075770fdc3cc
효정 안 <zzoo30@naver.com>
2021-10-08 16:45:22 +0900

    메시지박스 텍스트 수정

--- GitHub/Form1.cs
+++ GitHub/Form1.cs
@@ -19,7 +19,7 @@ namespace GitHub
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("눌렀습니다.");      
+            MessageBox.Show("눌렀어요!");      
         }
     }
 }
